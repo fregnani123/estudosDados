4c1b2f3b90dcf4ef2638ba037a4425668948d534
fregnani123 <fabianofregnani@hotmail.com>
2023-08-01 20:38:57 -0300

    estudos

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 
 
 
+function rand(){
+ const s = parseInt(Math.random() * 3)
+   return s <= 0 ? Number(s + 1) : s 
+}
 
+function simulaPromise(msg,tempo){
+ return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+     if(typeof msg !== 'string'){
+        reject('CAI NO ERRO');
+        return
+     }
+    resolve(msg + " Passei na promisse");
+    return
 
+    },tempo)
+ })
 
+}
 
+async function executa(){
+    const fase1 = await simulaPromise('fase 1', rand());
+    console.log(fase1)
+}
+executa()
 
 
 // function media(n1,n2){
@@ -16,4 +37,4 @@
 //     }
 // }
 
-// const aluno = media(7,8)
+// const aluno = media(7,8) 
