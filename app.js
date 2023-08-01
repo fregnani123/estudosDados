fb75f6c5c638752bbe2b95d4d3d08cc13294d008
fregnani123 <fabianofregnani@hotmail.com>
2023-08-01 17:59:17 -0300

    estudos

--- app.js
+++ app.js
@@ -4,3 +4,14 @@
 
 
 
+
+function media(n1,n2){
+    const media = (n1+n2)/2
+    if(media >= 7){
+        console.log(`Aluno com média:${media} aprovado`)
+    }else{
+        console.log(`Aluno com média:${media} reprovado`)
+    }
+}
+
+const aluno = media(7,8)
