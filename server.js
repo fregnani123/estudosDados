1e2613f4c16399d127f62eb32932cf7b7e21b07e
fregnani123 <fabianofregnani@hotmail.com>
2023-08-05 19:36:38 -0300

    estudos

--- server.js
+++ server.js
@@ -16,3 +16,18 @@ try {
     console.error('Erro ao iniciar o servidor:', error);
     process.exit(1);
 }
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}).connect((error)=>{
+    if(error)throw error;
+    console.log('conectado ao DB')
+});
+ 
+
+module.exports = connection;
