da0c979246ba5dce1c1aaef58f754a899797c401
fregnani123 <fabianofregnani@hotmail.com>
2023-08-15 21:43:43 -0300

    estudos

--- laco.js
+++ laco.js
@@ -1,7 +1,25 @@
 
-let n = 1
+// let n = 1
 
-for (let i = n; i <= 10; ++i){
+// for (let i = n; i <= 10; ++i){
    
-  console.log(i)
+//   console.log(i)
+// }
+
+
+// let i = 10
+
+// while (i < 15 ) {
+//     ++i;
+//     console.log(i)
+// }
+
+
+let nome = ["fabiano", "fernanda", "adair"];
+let novo = [];
+
+for (let i = 0; i < nome.length; i++) {
+     novo.push(nome[i].toUpperCase());
 }
+
+console.log(novo); // Output: ["FABIANO", "FERNANDA", "ADAIR"]
