7261c274b766cfa50c4895d3bc69211918fa6d16
fregnani123 <fabianofregnani@hotmail.com>
2023-08-17 20:21:44 -0300

    estudos

--- laco.js
+++ laco.js
@@ -1,4 +1,31 @@
 
+const pessoas = [
+     { id: 1, nome: 'João', idade: 25, cidade: 'São Paulo' },
+     { id: 2, nome: 'Maria', idade: 30, cidade: 'Rio de Janeiro' },
+     { id: 3, nome: 'Pedro', idade: 22, cidade: 'Belo Horizonte' },
+     { id: 4, nome: 'Ana', idade: 28, cidade: 'Curitiba' },
+     { id: 5, nome: 'Lucas', idade: 24, cidade: 'Porto Alegre' },
+];
+
+for (let i of pessoas) {
+     console.log(i)
+}
+
+const somaIdades = pessoas.reduce((total, pessoa, index, array) => total + pessoa.idade, 0);
+console.log();
+
+
+// const idades = pessoas.map(pessoas => pessoas);
+
+// console.log(idades)
+
+// const pessoasFilter = pessoas.filter(pessoa => pessoa.cidade = 'Rio de Janeiro');
+// console.log(pessoasFilter)
+
+
+
+
+
 // let n = 1
 
 // for (let i = n; i <= 10; ++i){
@@ -15,11 +42,23 @@
 // }
 
 
-let nome = ["fabiano", "fernanda", "adair"];
-let novo = [];
+// let nome = ["fabiano", "fernanda", "adair"];
+// let novo = [];
 
-for (let i = 0; i < nome.length; i++) {
-     novo.push(nome[i].toUpperCase());
-}
+// for (let i = 0; i < nome.length; i++) {
+//      novo.push(nome[i].toUpperCase());
+// }
 
-console.log(novo); // Output: ["FABIANO", "FERNANDA", "ADAIR"]
+// console.log(novo); // Output: ["FABIANO", "FERNANDA", "ADAIR"]
+
+
+// const novoNomes = nome.forM
+
+
+
+
+
+
+
+
+
